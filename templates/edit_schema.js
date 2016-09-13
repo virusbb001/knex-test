648fbd9611e205507badbf92c7b9f7b4cadcd86b
virus <virusbb001a@gmail.com>
2016-09-14 00:32:18 +0900

    change template

--- templates/edit_schema.js
+++ templates/edit_schema.js
@@ -1,15 +1,16 @@
 // See http://knexjs.org/#Schema-table to edit your table
+// If you want to cancel, do not edit and just quit your editor
 
 function change_schema(knex,table_name){
+  /*
   return knex.transaction(function(trx){
     // table change here
     // example:
-    /*
     return trx.schema.table(table_name, function(table){
       table.integer("priority").unsigned();
     });
-    */
   });
+  */
 }
 
 module.exports = change_schema;
