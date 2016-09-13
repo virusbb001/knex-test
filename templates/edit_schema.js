a959e93120d54ea71852d7071b2b6a241fbe57b8
virus <virusbb001a@gmail.com>
2016-09-14 08:35:24 +0900

    change template

--- templates/edit_schema.js
+++ templates/edit_schema.js
@@ -1,11 +1,12 @@
 // See http://knexjs.org/#Schema-table to edit your table
-// If you want to cancel, do not edit and just quit your editor
+// If you want to cancel, do not edit anything and just quit editor
 
+// available type is
+// integer, text, string, float, decimal, boolean,
+// date, dateTime, time, timestamp, binary, enum, other various...
 function change_schema(knex,table_name){
   /*
   return knex.transaction(function(trx){
-    // table change here
-    // example:
     return trx.schema.table(table_name, function(table){
       table.integer("priority").unsigned();
     });
