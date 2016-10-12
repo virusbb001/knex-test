d5a830518c39fd33d321515e1721c86b2402dae1
virus <virusbb001a@gmail.com>
2016-10-13 01:21:30 +0900

    add create_table

--- src/Core/Model/SchemaFields.js
+++ src/Core/Model/SchemaFields.js
@@ -1,3 +1,4 @@
+/* eslint no-console: off */
 import KnexModel from "./Knex";
 
 class SchemaField extends KnexModel{
@@ -13,6 +14,33 @@ class SchemaField extends KnexModel{
   saved_field(table){
     return this.knex.select().from(this.table_name).where({
       table_name: table
+    });
+  }
+
+  create_table(table_class){
+    var fields = table_class.fields;
+    return this.knex.schema.createTable(table_class.table_name, function(t){
+      for(var field in fields){
+        if(field == "#unique"){
+          fields[field].forEach(unique_field => {
+            t.unique(unique_field);
+          });
+          continue;
+        }
+        switch(fields[field]){
+          case "int":
+            t.integer(field);
+            break;
+          case "string":
+            t.string(field);
+            break;
+          case "text":
+            t.text(field);
+            break;
+          default:
+            throw new Error("not implemented yet");
+        }
+      }
     });
   }
 }
