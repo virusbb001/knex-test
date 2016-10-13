a404cb4745cfab27865e0f86104d7c75ae5cfeea
virus <virusbb001a@gmail.com>
2016-10-14 01:22:18 +0900

    add self check function

--- src/Core/Model/SchemaFields.js
+++ src/Core/Model/SchemaFields.js
@@ -43,6 +43,44 @@ class SchemaField extends KnexModel{
       }
     });
   }
+
+  self_schema_check(){
+    return this.knex(this.table_name).
+      distinct("table_name").select().then(tables => {
+        return Promise.all(tables.map(table_rows => {
+          var table = table_rows.table_name;
+          var actual_schema=this.knex(table).columnInfo().then(info => {
+            return Object.keys(info);
+          });
+          var expect_schema=this.knex(this.table_name).
+            select("field_name").where({
+              table_name: table
+            }).then(rows=>{
+              return rows.map(row => row.field_name);
+            });
+          return Promise.all([actual_schema, expect_schema]).then(values=>{
+            var actual_fields=values[0];
+            var expect_fields=values[1];
+
+            var actual_only_fields=actual_fields.filter(
+              field => (expect_fields.indexOf(field) < 0)
+            );
+            var expect_only_fields=expect_fields.filter(
+              field => (actual_fields.indexOf(field) < 0)
+            );
+
+            if(actual_only_fields.length > 0 || expect_only_fields.length > 0){
+              var msg="difference detected\n" +
+                (actual_only_fields.length > 0 ?
+                  "actual: "+actual_only_fields.join(" ")+"\n" : "") +
+                (expect_only_fields.length > 0 ?
+                  "expect: "+expect_only_fields.join(" ")+"\n" : "");
+              throw new Error(msg);
+            }
+          });
+        }));
+      });
+  }
 }
 
 export default SchemaField;
